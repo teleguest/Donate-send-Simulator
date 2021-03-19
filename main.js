70e7ad333e2a4f941f88552d0a553981f69be382
teleguest <56689167+teleguest@users.noreply.github.com>
2021-03-19 12:02:20 +0100

    add switch and case

--- main.js
+++ main.js
@@ -1,30 +1,28 @@
-function suma(){
-    var donate = document.getElementById("kwota").value;
-    if (donate == "5"){
-        alert("wysłałeś donate o wartości 5 zł tak mało dałeś?")
+function sum(){
+    var donate = document.getElementById("amount").value;
+    switch(donate){
+        case "5":
+            alert("wysłałeś donate o wartości 5 zł tak mało dałeś?");
+            break;
+        case "10":
+            alert("wysłałeś donate o wartości 10 zł za tyle to se kupisz usb z bazaru?");
+            break;
+        case "15":
+            alert("wysłałeś donate o wartości 15 zł możesz w końcu kupić kole");
+            break;
+        case "20":
+            alert("wysłałeś donate o wartości 20 zł w końcu kebab")
+            break;
+        case "50":
+            alert("wysłałeś donate o wartości 50 zł Czuje Stonks");
+            var cs = new Audio('music/coming-stonks.mp3');
+            cs.play();
+            break;
+        case "100":
+            alert("wysłałeś donate o wartości 100 zł STONKS!");
+            var stonks = new Audio('music/stonks.mp3');
+            stonks.play();
+            break;
     }
-    if (donate == "10"){
-        alert("wysłałeś donate o wartości 10 zł za tyle to se kupisz usb z bazaru");
-    }
-    if (donate == "15"){
-        alert("wysłałeś donate o wartości 15 zł możesz w końcu kupić kole");
-    }
-    if (donate == "20"){
-        alert("wysłałeś donate o wartości 20 zł w końcu kebab");
-    }
-    if (donate == "50"){
-        alert("wysłałeś donate o wartości 50 zł Czuje Stonks");
-        var cs = new Audio('music/coming-stonks.mp3');
-        cs.play();
-    }
-    if (donate == "100"){
-        alert("wysłałeś donate o wartości 100 zł STONKS!");
-        var stonks = new Audio('music/stonks.mp3');
-        stonks.play();
-        document.getElementById("imgstonks").src = "image/stonks.jpeg";
-    }
-    function dalatestonks(){
-    document.getElementById("imgstonks").src = "";
-}
 }
 // Widzisz ten kod brawo
